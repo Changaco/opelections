ca201245c7dc0798b8360d0727c8075253187097
Changaco <changaco@changaco.oy.lc>
2012-03-26 19:23:22 +0200

    listen on a UNIX socket in production

--- main.hs
+++ main.hs
@@ -1,8 +1,23 @@
-import Prelude              (IO)
-import Yesod.Default.Config (fromArgs)
-import Yesod.Default.Main   (defaultMain)
+import Prelude
+import Yesod.Default.Config
+import Yesod.Logger         (defaultDevelopmentLogger)
 import Settings             (parseExtra)
 import Application          (getApplication)
+import Network.Socket
+import Network.Wai.Handler.Warp
+    (runSettings, runSettingsSocket, defaultSettings, settingsPort)
 
 main :: IO ()
-main = defaultMain (fromArgs parseExtra) getApplication
+main = do
+    config <- fromArgs parseExtra
+    logger <- defaultDevelopmentLogger
+    app <- getApplication config logger
+    let settings = defaultSettings { settingsPort = appPort config }
+    if appPort config == 0
+       then do
+            sock <- socket AF_UNIX Stream defaultProtocol
+            bindSocket sock $ SockAddrUnix "/tmp/opelections.sock"
+            listen sock 1
+            runSettingsSocket settings sock app
+       else
+            runSettings settings app
